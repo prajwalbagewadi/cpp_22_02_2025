4ad5e0e75cf823dfc0f939434d5a9f13db09dcd9
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-02-22 11:29:57 +0530

    init cpp resolve

---
+++ 00_hello/hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream> // header file.
+using namespace std; // namespace defined or scope resolution to std.
+int main(){
+    std::cout<<"hello"<<"class"<<"\n"; // std::cout(scope resolution to std.) :: console.output.stream
+    cout<<"helloworld";
+    return 0;
+}
---
+++ 01_input_output/io.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //output
+    cout<<"message to insert into cout"<<"\n"; //ostream obj cout
+
+    //input
+    int num;
+    cout<<"enter the number:";
+    // obj << "val" val is copied into object; (<<) directional operators
+    cin>>num; //istream obj cin
+    // obj >> "var" obj val is copied into variable or obj; (>>) directional operators
+    cout<<"num="<<num;
+
+    //arthimetic operators. (hw.)
+    return 0;
+}
